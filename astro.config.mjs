5109324643e367494485a7d166d083384d88fc40
thupten tsundue <thups1951@gmail.com>
2025-12-17 15:11:07 -0500

    code refactor for css

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://raywahygiene.github.io",
   base: "/rewa",
+  integrations: [tailwind()],
 });
